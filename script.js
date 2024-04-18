e0f6446f6f5981b7fbc0d25656df39355b97ca10
vbratsyun <vbratsyun@yandex.ru>
2024-04-18 19:45:12 +0300

    добавление изменение цвета в файл script.js

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ const onRangeBarInput = () => {
    const rangeLabel = document.querySelector('.article__range-label output');
    rangeLabel.textContent = ` ${rangeBar.value}px`;
    text.style.fontSize = rangeLabel.textContent;
+
+   //Динамическое изменение цветов при помощи JavaScript
+   const value = (rangeBar.value-rangeBar.min)/(rangeBar.max-rangeBar.min)*100;
+   rangeBar.style.background = `linear-gradient(to right, rgb(106, 90, 205, 0.8) ${value}%, rgb(255, 255, 255) ${value}%)`;
 }
 
 rangeBar.addEventListener('input', onRangeBarInput);
